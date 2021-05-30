c06d304391f3a12d956b0d97e054092e75d5e8b6
liweikang <liweikang@foresee.com.cn>
2021-05-30 22:56:24 +0800

    Site updated: 2021-05-30 22:56:21

--- photos/ins.js
+++ photos/ins.js
@@ -1,4 +1,4 @@
-/******/
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\css\APlayer.min.css"><script src="\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\js\Meting.min.js"></script>/******/
 (function(modules) { // webpackBootstrap
   /******/ // The module cache
   /******/
@@ -124,7 +124,7 @@
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src= "/img/loading.gif" data-lazy-src="/assets/img/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
